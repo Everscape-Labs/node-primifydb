2f5a4d28b5fb402eb35ec8ebb2f71ec7cc38f5d4
johan-zelearner <zelearner@gmail.com>
2018-08-20 09:38:17 +0200

    Replace import statements white `require` Export the Primify class in the main file

--- dist/Primify.js
+++ dist/Primify.js
@@ -6,13 +6,9 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _got = require('got');
-
-var _got2 = _interopRequireDefault(_got);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var got = require('got');
 
 var HttpAgent = require('agentkeepalive');
 
@@ -61,7 +57,7 @@ var Primify = function () {
 
       // console.log('WRITING', url);
 
-      return _got2.default.post(url, {
+      return got.post(url, {
         agent: {
           http: client
         },
@@ -103,7 +99,7 @@ var Primify = function () {
 
       console.log('READING ' + url, payload);
 
-      return _got2.default.post(url, {
+      return got.post(url, {
         json: true,
         body: payload
       }).then(function (response) {
@@ -128,7 +124,7 @@ var Primify = function () {
 
       var url = this.url + '/' + this.database + '/_write/bulk/' + collection;
 
-      return _got2.default.post(url, {
+      return got.post(url, {
         agent: {
           http: client
         },
